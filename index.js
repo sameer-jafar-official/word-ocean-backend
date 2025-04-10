7fd29f6f3afd1938bc1d141ef16854fc2082efbc
sameer-jafar-official <sameer.jafar.official@gmail.com>
2025-04-11 02:30:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotev').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("connected to MongoDB"));
+
+const WordSchema = new mongoose.Schema({
+    word: String,
+    meaning:String,
+    example:String,
+    createdAt : { type : Date , default : Date.now}
+});
+const word = mongoose.model('Word', WordSchema);
+
+app.post('/add-word' ,async (req , res) => {
+    const {word , meaning , example} = req.body;
+    const entry = new WordSchema({word , meaning , example });
+    await entry.save();
+    res.json({message : 'Word Added!' });
+});
+
+app.get('/words' , async (req , res)=>{
+    const words = await WordSchema.find();
+    res.json(words);
+});
+app.listen(3000 , ()=> console.log("server running on port 3000"));
